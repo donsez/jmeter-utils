99e6c1d9c1549687e31d44e708e28b68a09901cc
Didier Donsez <didier.donsez@gmail.com>
2014-04-12 09:32:53 +0200

    add scripts for generating and executing a batch of JMeter test plans

---
+++ script/generateTestPlans.sh
@@ -0,0 +1,17 @@
+
+ANUMCLIENT=( 100 1000 10000)
+AQOS=(0 1 2)
+ARETAIN=(true false)
+
+for NUMCLIENT in ${ANUMCLIENT[*]}
+do
+for QOS in ${AQOS[*]}
+do
+for RETAIN in ${ARETAIN[*]}
+do
+        SUFFIX=$NUMCLIENT.$QOS.$RETAIN
+	generating testplan.$SUFFIX.jmx
+	sed  's/__NUMCLIENT__/$NUMCLIENT/g' TestPlan.tmpl.jmx | sed 's/__QOS__/$QOS/g'  | sed 's/__RETAIN__/$RETAIN/g'  > testplan.$SUFFIX.jmx
+done
+done
+done
